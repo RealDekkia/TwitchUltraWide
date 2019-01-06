618df8c0e7c9643a3fc04ce6d13fb3979769dffd
RealDekkia <bresleinm@gmail.com>
2019-01-06 19:54:31 +0100

    Update customScript.js

--- customScript.js
+++ customScript.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @author       https://github.com/RealDekkia/
-// @match        https://www.twitch.tv/gamesdonequick
+// @match        https://www.twitch.tv
 // ==/UserScript==
 
 (function() {
